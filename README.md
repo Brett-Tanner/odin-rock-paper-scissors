944dcabb70a637f83778b258f84b902bd38357f1
Brett <bretttanner171@gmail.com>
2022-04-13 15:58:51 +0900

    Complete basic code

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # odin-rock-paper-scissors
 
-- Have a collapsible activity log
-		- Result of current round is displayed in a window created at the end of each round and deleted at the beginning of the next
-		- make the buttons react and play a sound when clicked (js30 project 1 does this)
-		- Add a name field that seems to transform into a div with the player's name inside
+- Make activity log collapsible
+- Add a name field that seems to transform into a div with the player's name inside
+- create a new game button that zeroes everything out and lets you start a new game
--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+// select elements
 const playButtons = document.querySelectorAll(".play");
-playButtons.addEventListener('click', playRound(e));
-
-function getPlayerChoice() {
-    // Prompts for player's choice and checks for a valid input
-    
-    playerChoice = prompt("Choose your fighter! Rock, Paper or Scissors?").toUpperCase();
-
-    if (playerChoice !== "ROCK" && playerChoice !== "PAPER" && playerChoice !== "SCISSORS") {
-        alert("Invalid input");
-        getPlayerChoice();
-    }
+// return the id of the clicked button for use in playRound()
+for (i = 0; i < 3; i++) {
+    playButtons[i].addEventListener('click', function(e) {
+        playRound(e);
+    });
 }
 
+const resultDisplay = document.querySelector("#resultDisplay");
+const runningScore = document.querySelector("#runningScore");
+const actionLog = document.querySelector("#actionLog");
+
+// keep score
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     // Randomly selects the computer's choice from an array
@@ -25,29 +27,75 @@ function getComputerChoice() {
 function playRound(e) {
     // Get choices
 
-    playerChoice = e.which;
+    playerChoice = e.target.id;
     getComputerChoice();
     
 
-    console.log(playerChoice)
-    console.log(computerChoice)
+    // exit if game is already over
+    if (playerScore >= 5 || computerScore >= 5) {
+        return;
+    }
 
+  
     /* compare choices and return result (remember it's important to return the result of recursive functions) */
 
     if (playerChoice === computerChoice) {
-        alert(`Both players chose ${playerChoice.toLowerCase()}! Try again.`);
+        const result = (`Both players chose ${playerChoice.toLowerCase()}! Try again.`);
+
+        // remove text of result box and add text for this round
+        resultDisplay.textContent = result;
+
+        // add it to the activity log
+        actionLog.textContent = `${actionLog.textContent}
+        ${result}`;
+
+        // start a rematch
         return playRound();
     }
         else if (playerChoice === "ROCK" && computerChoice === "SCISSORS" || playerChoice === "PAPER" && computerChoice === "ROCK" || playerChoice === "SCISSORS" && computerChoice === "PAPER") {
-            console.log(`${playerChoice.charAt(0)}${playerChoice.slice(1).toLowerCase()} beats ${computerChoice.toLowerCase()}! You win this round.`);
-            return "PlayerWins";
+            const result = (`${playerChoice.charAt(0)}${playerChoice.slice(1).toLowerCase()} beats ${computerChoice.toLowerCase()}! You win this round.`);
+            
+            // remove text of result box and add text for this round
+            resultDisplay.textContent = result;
+
+            // add it to the activity log
+            actionLog.textContent = `${actionLog.textContent}
+            ${result}`;
+            
+            // update the running total
+            playerScore++;
+            runningScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
         }
         else if (playerChoice === "SCISSORS" && computerChoice === "ROCK" || playerChoice === "ROCK" && computerChoice === "PAPER" || playerChoice === "PAPER" && computerChoice === "SCISSORS") {
-            console.log(`${computerChoice.charAt(0)}${computerChoice.slice(1).toLowerCase()} beats ${playerChoice.toLowerCase()}! Better luck next time.`);
-            return "ComputerWins";
+            const result = (`${computerChoice.charAt(0)}${computerChoice.slice(1).toLowerCase()} beats ${playerChoice.toLowerCase()}! Better luck next time.`);
+            
+            // remove text of result box and add text for this round
+            resultDisplay.textContent = result;
+
+            // add it to the activity log
+            actionLog.textContent = `${actionLog.textContent}
+            ${result}`;
+            
+            // update the running total
+            computerScore++;
+            runningScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
         }
 
+    // check if the game is over
+    if (playerScore >= 5) {
+        resultDisplay.setAttribute("class", "victory");
+        resultDisplay.textContent = `Congratulations, robot uprising averted! You win ${playerScore} to ${computerScore}`;
+        return;
+    }
+    else if (computerScore >= 5) {
+        resultDisplay.setAttribute("class", "defeat");
+        resultDisplay.textContent = `The machine uprising begins... You lose ${computerScore} to ${playerScore}`;
+        return;
+    }
+
 }
+
+
 
 
 /* 
