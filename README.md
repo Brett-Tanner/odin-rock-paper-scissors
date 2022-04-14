732edb65ca51260b0372ba90b9caab6873dd4cce
Brett <bretttanner171@gmail.com>
2022-04-14 21:59:15 +0900

    Add reset button

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # odin-rock-paper-scissors
 
-- Create a new game button that zeroes everything out and lets you start a new game - still need to make it do anything
 - Add a title at the top
 - Make it all look pretty
 - Make activity log collapsible
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// score variables
+let playerScore = 0;
+let computerScore = 0;
+
 // select elements
 const playButtons = document.querySelectorAll(".play");
 // return the id of the clicked button for use in playRound()
@@ -10,11 +14,14 @@ for (i = 0; i < 3; i++) {
 const resultDisplay = document.querySelector("#resultDisplay");
 const runningScore = document.querySelector("#runningScore");
 const actionLog = document.querySelector("#actionLog");
- 
+const resetButton = document.querySelector("#resetButton");
 
-// score variables
-let playerScore = 0;
-let computerScore = 0;
+
+// Run resetGame when reset button is clicked
+resetButton.addEventListener("click", function(e) {
+    resetGame();
+});
+
 
 function getComputerChoice() {
     // Randomly selects the computer's choice from an array
@@ -90,8 +97,7 @@ function playRound(e) {
         resultDisplay.setAttribute("class", "victory");
         resultDisplay.textContent = `Congratulations, robot uprising averted! You win ${playerScore} to ${computerScore}`;
         // show option to play again
-        const gameReset = document.querySelector("#gameReset");
-        gameReset.removeAttribute("class");
+        resetButton.removeAttribute("class");
         return;
     }
     else if (computerScore >= 5) {
@@ -99,9 +105,17 @@ function playRound(e) {
         resultDisplay.setAttribute("class", "defeat");
         resultDisplay.textContent = `The machine uprising begins... You lose ${computerScore} to ${playerScore}`;
         // show option to play again
-        const gameReset = document.querySelector("#gameReset");
-        gameReset.removeAttribute("class");
+        resetButton.removeAttribute("class");
         return;
     }
 
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    resultDisplay.textContent = "";
+    actionLog.textContent = "";
+    runningScore.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+    resetButton.setAttribute("class", "hidden");
 }
